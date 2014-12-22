7513b2d03ac201523dee3a75823ae7fb4728264a
Vitalii Abetkin <abvit89@gmail.com>
2014-12-22 03:14:49 +0300

    go

--- appunits/__init__.py
+++ appunits/__init__.py
@@ -1,14 +1,41 @@
-from .util.mro import mro
+from .util.mro import _mro
+
 
 class AppUnit(object):
+
+    deps = ()
+
+    def __init__(self, deps=None, identity=None):
+        if deps is not None:
+            self.deps = deps
+        if identity is not None:
+            self.identity = identity 
+        assert self.identity is not None, "App unit should have an identity."
     
-    def __init__(self, deps):
-        1
+    # FIXME view is always the context
+
+    def _get_parents(self):
+        return self.deps # TODO not always
+
+    @staticmethod
+    def _get_pro(obj):
+        return getattr(obj, '__pro__', ())
+
+    def run(self):
+        self.__pro__ = _mro(self._get_parents(), get_mro=self._get_pro())
+        return self()
+
+    @classmethod
+    def run_units(cls, *units):
+        unit = 0
+        issubclass(unit, AppUnit)
 
 
 class ContextAttribute:
     '''
-    An attribute that is looked up in the context.
+    An attribute that is looked up in the (parent) context.
+
+    "__pro__" stays for "parent resolution order". 
     '''
 
     PUBLISHED_CONTEXT_ATTR = 'published_context'
@@ -20,6 +47,7 @@ class ContextAttribute:
     def __get__(self, instance, owner):
         if instance:
             return self._lookup(instance)
+        return self
 
     def _lookup(self, instance):
         for obj in instance.__pro__:
@@ -31,20 +59,3 @@ class ContextAttribute:
             if self.name in published_context_extra:
                return obj.published_context_extra[self.name]
         raise AttributeError(self.name)
-    
-
-
-#class PrototypeMixin:
-#    published_context = ()
-
-#    def get_from_context(self, attr, *default):
-#        for obj in self._objects():
-#            if attr in obj.published_context:
-#                return getattr(obj, attr)
-#            if hasattr(obj, 'published_context_extra') \
-#                    and attr in obj.published_context_extra:
-#                return obj.published_context_extra[attr]
-#        if default:
-#            return default[0]k
-#        raise AttributeError(attr)
-
