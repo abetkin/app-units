8010012a4b512410e8a63d35edd1c4caeab17070
Vitalii Abetkin <abvit89@gmail.com>
2015-01-14 02:27:23 +0300

    go

--- examples/aprentproj/aprentproj/settings.py
+++ examples/aprentproj/aprentproj/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'aprent',
+    'django_extensions',
 )
 
 MIDDLEWARE_CLASSES = (
